10433d247b81fc8ab54e27a4561716ff0e56f865
MTSyntho <phenyosethole0907@gmail.com>
2024-11-25 23:31:20 +0200

    Setup being reworked

---
+++ setup/setup.js
@@ -0,0 +1,9 @@
+/*
+	Setup Script for AzuOS
+	Written by: MTSyntho @ AzuSystem 2024
+*/
+
+setTimeout(function() {
+	var background = document.getElementById('background');
+	background.classList.add('background-animation');
+}, 1000);
